1a3abde58c4933498eb0a87c4575bb193dc9515a
aakash G <aakashganeshv123@gmail.com>
2025-09-18 14:40:39 +0530

    Add files via upload

---
+++ con q4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(4-3)
+	printf("%d is odd",2);
+	else
+	printf("%d is even",4);
+	return 0;
+}
---
+++ con q5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	if('a is three digit')
+	printf("%d is three digit",);
+	else
+	printf("%d is not three digit",);
+	return 0;
+	
+}
